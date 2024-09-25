4de6e84a134eaabff428cc41ee0a9a62f08226c9
Victor-cmd19 <victor_detofano@estudante.sc.senai.br>
2024-09-25 00:22:30 +0000

    get all emprestimos

--- src/application/use-cases/get-all-emprestimo/get-emprestimos-usecase-output.ts
+++ src/application/use-cases/get-all-emprestimo/get-emprestimos-usecase-output.ts
@@ -1,1 +1,23 @@
-export type GetAllEmprestimoUseCaseOutput = {}
+type ItemOutput = {
+    id: string
+    name: string
+}
+
+type PessoaOutput = {
+    id: string
+    name: string
+}
+
+type usuarioOutput = {
+    id: string
+    name: string
+}
+
+export type GetAllEmprestimoUseCaseOutput = {
+    id: string; 
+    item: ItemOutput;
+    pessoa: PessoaOutput;
+    usuario: usuarioOutput;
+    dataEmprestimo: string;
+    dataDevolucao:string | undefined;
+}
