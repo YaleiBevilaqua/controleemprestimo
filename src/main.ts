8d597334282625d8c1bf20bb0190554b04468b74
MatheusBertan <mathebertan@gmail.com>
2024-10-02 23:31:23 +0000

    ADD pessoa-repository-memory

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import { ItemController } from './application/controller/item-controller';
 import { ItemRepositoryMemory } from './infra/repository/memory/item-repository-memory';
+import { PessoaController } from './application/controller/pessoa-controller';
+import { PessoaRepositoryMemory, PessoaRepositoryMemory } from './infra/repository/memory/pessoa-repository-memory';
 
 const app = express();
 const port = 3333;
@@ -8,7 +10,11 @@ app.use(express.json())
 
 const itemRepositoryMemory = new ItemRepositoryMemory()
 
+const pessoaRepositoryMemory = new PessoaRepositoryMemory()
+
 const itemsController = new ItemController(itemRepositoryMemory)
+
+const pessoaController = new PessoaController(pessoaRepositoryMemory)
 
 app.get('/items', (request, response) => {
     response.send(itemsController.getAll({}));
@@ -21,3 +27,7 @@ app.post('/items', (request, response) => {
 app.listen(port, () => {
     console.log("Servidor iniciado na porta " + port)
 })
+
+app.get('/pessoas', (request, response) => {
+    response.send(pessoaController.getAll({}));
+});
